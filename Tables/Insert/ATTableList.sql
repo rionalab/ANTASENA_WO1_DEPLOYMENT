23f81208f42956565da018639e740cfb047828c8
rionalab <rionalab@gmail.com>
2025-07-18 14:29:27 +0700

    init

---
+++ Tables/Insert/ATTableList.sql
@@ -0,0 +1,12 @@
+INSERT INTO [dbo].[ATTableList]
+   ([menudesc]
+   ,[tablename]
+   ,[ColKeterangan])
+VALUES
+   ('Pengaturan Tipe Periode Semesteran'
+           , 'ATMSTipePeriodeSemester'
+           , 'y.PeriodName'),
+   ('Pengaturan Tipe Periode Tahunan'
+           , 'ATMSTipePeriodeTahun'
+           , 'y.PeriodName')
+GO
