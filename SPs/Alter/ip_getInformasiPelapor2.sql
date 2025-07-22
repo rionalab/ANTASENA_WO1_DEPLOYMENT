94ccfe9acaa7c66cadaf773dfa976c5ebd231dd1
rionalab <rionalab@gmail.com>
2025-07-22 10:32:45 +0700

    format

--- SPs/Alter/ip_getInformasiPelapor2.sql
+++ SPs/Alter/ip_getInformasiPelapor2.sql
@@ -3,14 +3,14 @@ CREATE PROCEDURE [dbo].[ip_getInformasiPelapor2]
 AS
 BEGIN
 
-   SELECT
-      b.InstansiId,
-      a.IdPelapor,
-      a.IdPelapor + ' - ' + b.nama AS Nama,
-      b.jeniskegiatanoperasionalbank AS Jnskeg,
-      b.statuskantor AS Stat,
+    SELECT
+        b.InstansiId,
+        a.IdPelapor,
+        a.IdPelapor + ' - ' + b.nama AS Nama,
+        b.jeniskegiatanoperasionalbank AS Jnskeg,
+        b.statuskantor AS Stat,
 
-      '<p>' + STRING_AGG(  
+        '<p>' + STRING_AGG(  
             CONVERT(VARCHAR(MAX),  
                 CASE   
                     WHEN a.Periode = 'D'  THEN c.namainformasi + ' - Harian Normal'  
@@ -25,51 +25,51 @@ BEGIN
             ), '</p><p>'  
         ) WITHIN GROUP (ORDER BY c.namainformasi ASC) + '</p>' AS harian,
 
-      '<p>' + STRING_AGG(  
+        '<p>' + STRING_AGG(  
             CONVERT(VARCHAR(MAX),  
                 CASE WHEN a.Periode = 'W' THEN c.namainformasi ELSE NULL END  
             ), '</p><p>'  
         ) WITHIN GROUP (ORDER BY c.namainformasi ASC) + '</p>' AS mingguan,
 
-      '<p>' + STRING_AGG(  
+        '<p>' + STRING_AGG(  
             CONVERT(VARCHAR(MAX),  
                 CASE WHEN a.Periode = 'M' THEN c.namainformasi ELSE NULL END  
             ), '</p><p>'  
         ) WITHIN GROUP (ORDER BY c.namainformasi ASC) + '</p>' AS bulanan,
 
-      '<p>' + STRING_AGG(  
+        '<p>' + STRING_AGG(  
             CONVERT(VARCHAR(MAX),  
                 CASE WHEN a.Periode = 'Q' THEN c.namainformasi ELSE NULL END  
             ), '</p><p>'  
         ) WITHIN GROUP (ORDER BY c.namainformasi ASC) + '</p>' AS triwulanan,
 
-      '<p>' + STRING_AGG(  
+        '<p>' + STRING_AGG(  
             CONVERT(VARCHAR(MAX),  
                 CASE WHEN a.Periode = 'S' THEN c.namainformasi ELSE NULL END  
             ), '</p><p>'  
         ) WITHIN GROUP (ORDER BY c.namainformasi ASC) + '</p>' AS semesteran,
 
-      '<p>' + STRING_AGG(  
+        '<p>' + STRING_AGG(  
             CONVERT(VARCHAR(MAX),  
                 CASE WHEN a.Periode = 'A' THEN c.namainformasi ELSE NULL END  
             ), '</p><p>'  
         ) WITHIN GROUP (ORDER BY c.namainformasi ASC) + '</p>' AS tahunan
 
-   FROM PelaporInformation a
-      INNER JOIN MsInstansi b ON a.IdPelapor = b.idpelapor
-      LEFT JOIN (  
+    FROM PelaporInformation a
+        INNER JOIN MsInstansi b ON a.IdPelapor = b.idpelapor
+        LEFT JOIN (  
         SELECT DISTINCT idinformation, namainformasi
-      FROM metadata_mgmt
-      WHERE parentinformation IS NULL  
+        FROM metadata_mgmt
+        WHERE parentinformation IS NULL  
     ) AS c ON a.IdInformation = c.idinformation
 
-   GROUP BY    
+    GROUP BY    
         b.InstansiId,   
         a.IdPelapor,   
         b.nama,   
         b.jeniskegiatanoperasionalbank,   
         b.statuskantor
 
-   ORDER BY a.IdPelapor + ' - ' + b.nama;
+    ORDER BY a.IdPelapor + ' - ' + b.nama;
 
 END  
